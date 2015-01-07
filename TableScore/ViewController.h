04b9f61fd3ff842149b504db7e6d76a59dfa4f04
Will Richman <wrichman@gmail.com>
2015-01-07 15:21:59 -0800

    Initial Commit

---
+++ TableScore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableScore
+//
+//  Created by William Richman on 1/7/15.
+//  Copyright (c) 2015 Will Richman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TableScore/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TableScore
+//
+//  Created by William Richman on 1/7/15.
+//  Copyright (c) 2015 Will Richman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
