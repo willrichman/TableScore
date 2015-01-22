e612f99cb00b015da3adef29b9507a5edc43297d
Will Richman <wrichman@gmail.com>
2015-01-22 12:48:58 -0800

    Set up initial layout and tab bar.

---
+++ TableScore/HomeScoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeScoreViewController.h
+//  TableScore
+//
+//  Created by William Richman on 1/14/15.
+//  Copyright (c) 2015 Will Richman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeScoreViewController : UIViewController
+
+@end
---
+++ TableScore/HomeScoreViewController.m
@@ -0,0 +1,27 @@
+//
+//  HomeScoreViewController.m
+//  TableScore
+//
+//  Created by William Richman on 1/14/15.
+//  Copyright (c) 2015 Will Richman. All rights reserved.
+//
+
+#import "HomeScoreViewController.h"
+
+@interface HomeScoreViewController ()
+
+@end
+
+@implementation HomeScoreViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
